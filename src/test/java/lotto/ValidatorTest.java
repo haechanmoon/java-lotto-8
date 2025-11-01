43230960cdc32eff05282a53cf8d2b9d88a72b36
haechanmoon <timmymoon@naver.com>
2025-11-01 22:23:18 +0900

    fix(ValidatorTest) : DisplayName 복붙한거 수정

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -8,35 +8,35 @@ import org.junit.jupiter.api.Test;
 class ValidatorTest {
 
     @Test
-    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외 발생 후 재입력 해야한다.")
-    void 구입_금액이_1000_단위가_아닐_때(){
+    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외가 발생한다.")
+    void 구입_금액이_1000_단위가_아닐_때() {
         //given
         String purchaseAmount = "1500";
 
         //when&then
-        assertThatThrownBy(()-> Validator.validatePurchaseAmount(purchaseAmount))
+        assertThatThrownBy(() -> Validator.validatePurchaseAmount(purchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외 발생 후 재입력 해야한다.")
-    void 구입_금액이_숫자가_아닐_때(){
+    @DisplayName("구입 금액이 숫자가 아닐 경우 예외가 발생한다.")
+    void 구입_금액이_숫자가_아닐_때() {
         //given
         String purchaseAmount = "abc";
 
         //when&then
-        assertThatThrownBy(()-> Validator.validatePurchaseAmount(purchaseAmount))
+        assertThatThrownBy(() -> Validator.validatePurchaseAmount(purchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외 발생 후 재입력 해야한다.")
-    void 구입_금액이_0이하의_정수일때(){
+    @DisplayName("구입 금액이 0 이하의 정수일 경우 예외가 발생한다.")
+    void 구입_금액이_0이하의_정수일때() {
         //given
         String purchaseAmount = "-2000";
 
         //when&then
-        assertThatThrownBy(()-> Validator.validatePurchaseAmount(purchaseAmount))
+        assertThatThrownBy(() -> Validator.validatePurchaseAmount(purchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
