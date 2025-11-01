1d9040d02dd7a6da48312a65c522df77e08fa67e
haechanmoon <timmymoon@naver.com>
2025-11-01 22:25:42 +0900

    refactor (Validator) : 0이하의 정수 먼저로 검사 순서 변경

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -9,11 +9,12 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
         }
 
-        if ((purchaseAmount % 1000) != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해야합니다");
-        }
         if (purchaseAmount <= 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위의 양수를 입력해야합니다.");
+        }
+
+        if ((purchaseAmount % 1000) != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해야합니다");
         }
     }
 }
