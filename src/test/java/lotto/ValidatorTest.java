cdcaa021d000da16279a9706669855bddd936967
haechanmoon <timmymoon@naver.com>
2025-11-01 22:20:28 +0900

    test(ValidatorTest) : 구입 금액이 0이하의 정수일 때 (`"-2000"`) 예외 발생

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -28,4 +28,15 @@ class ValidatorTest {
         assertThatThrownBy(()-> Validator.validatePurchaseAmount(purchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외 발생 후 재입력 해야한다.")
+    void 구입_금액이_0이하의_정수일때(){
+        //given
+        String purchaseAmount = "-2000";
+
+        //when&then
+        assertThatThrownBy(()-> Validator.validatePurchaseAmount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
