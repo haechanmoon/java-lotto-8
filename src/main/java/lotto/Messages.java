0eca98622f9c1ea924139737cd2adfdda4544e12
haechanmoon <timmymoon@naver.com>
2025-11-02 22:20:36 +0900

    chore(Messages) : Lotto 에러문구 추가

--- src/main/java/lotto/Messages.java
+++ src/main/java/lotto/Messages.java
@@ -7,9 +7,11 @@ public class Messages {
 
     static final String VIEW_PURCHASE_TOTAL_LOTTO = "개를 구매했습니다.";
 
+    static final String ERROR_LOTTO_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    static final String ERROR_LOTTO_NUMBER_DUPLICATE = "[ERROR] 로또 번호에 중복된 숫자가 있습니다.";
+    static final String ERROR_LOTTO_NUMBER_COUNT = "[ERROR] 당첨 번호는 6개여야 합니다.";
     static final String ERROR_WINNING_NUMBER_COUNT = "[ERROR] 당첨 번호는 6개여야 합니다.";
     static final String ERROR_NUMBER_NOT_NUMBER = "[ERROR] 로또 번호는 숫자만 입력 가능합니다.";
-    static final String ERROR_LOTTO_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     static final String ERROR_WINNING_NUMBER_DUPLICATE = "[ERROR] 당첨 번호에 중복된 숫자가 있습니다.";
     static final String ERROR_BONUS_NUMBER_DUPLICATE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
 }
