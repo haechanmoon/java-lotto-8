5a9334a7a2e7ab38918c1c9db21bbc869be414f2
haechanmoon <timmymoon@naver.com>
2025-11-02 23:52:13 +0900

    test(LottoRank) : 4개 일치 시 4등을 반환 테스트

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -9,7 +9,7 @@ class LottoRankTest {
 
     @Test
     @DisplayName("6개 일치 시 1등을 반환한다")
-    void valueOf_6_matches() {
+    void 여섯개_일치_시_1등을_반환() {
         // given
         int matchCount = 6;
         boolean bonusMatch = false;
@@ -24,7 +24,7 @@ class LottoRankTest {
 
     @Test
     @DisplayName("5개 일치 + 보너스 일치 시 2등을 반환한다")
-    void valueOf_5_matches_with_bonus() {
+    void 다섯개_일치와_보너스_일치_시_2등을_반환() {
         // given
         int matchCount = 5;
         boolean bonusMatch = true;
@@ -39,7 +39,7 @@ class LottoRankTest {
 
     @Test
     @DisplayName("5개 일치 + 보너스 불일치 시 3등을 반환한다")
-    void valueOf_5_matches_without_bonus() {
+    void 다섯개_일치와_보너스_불일치_시_3등을_반환() {
         // given
         int matchCount = 5;
         boolean bonusMatch = false;
@@ -49,6 +49,21 @@ class LottoRankTest {
 
         // then
         assertThat(rank).isEqualTo(LottoRank.THIRD);
+        assertThat(rank.getWinnings()).isEqualTo(1_500_000);
     }
 
+    @Test
+    @DisplayName("4개 일치 시 4등을 반환한다")
+    void 네개_일치_시_4등을_반환() {
+        // given
+        int matchCount = 4;
+        boolean bonusMatch = false;
+
+        // when
+        LottoRank rank = LottoRank.valueOf(matchCount, bonusMatch);
+
+        // then
+        assertThat(rank).isEqualTo(LottoRank.FOURTH);
+        assertThat(rank.getWinnings()).isEqualTo(50_000);
+    }
 }
