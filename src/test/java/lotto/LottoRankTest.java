dd8a0089e3074e9a8996df49254ce795664b6f1a
haechanmoon <timmymoon@naver.com>
2025-11-02 23:41:40 +0900

    test(LottoRank) :5개 일치 + 보너스 일치 시 2등 반환

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -21,4 +21,19 @@ class LottoRankTest {
         assertThat(rank).isEqualTo(LottoRank.FIRST);
         assertThat(rank.getWinnings()).isEqualTo(2_000_000_000);
     }
+
+    @Test
+    @DisplayName("5개 일치 + 보너스 일치 시 2등을 반환한다")
+    void valueOf_5_matches_with_bonus() {
+        // given
+        int matchCount = 5;
+        boolean bonusMatch = true;
+
+        // when
+        LottoRank rank = LottoRank.valueOf(matchCount, bonusMatch);
+
+        // then
+        assertThat(rank).isEqualTo(LottoRank.SECOND);
+        assertThat(rank.getWinnings()).isEqualTo(30_000_000);
+    }
 }
