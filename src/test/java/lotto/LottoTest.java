4a7795284a879836bfe836a8db7c3798087d8e83
haechanmoon <timmymoon@naver.com>
2025-11-02 23:20:28 +0900

    test(LottoTest) :로또 번호가 올바르게 오름차순 정렬되는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,25 +1,38 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class LottoTest {
+
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+    @DisplayName("로또 번호의 개수가 6개가 아니면 예외가 발생한다.")
+    void 로또_번호의_개수가_6개가_아니면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호가 올바르게 오름차순 정렬되는지 테스트")
+    void getSortedNumbersTest() {
+        // given
+        Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+
+        // when
+        List<Integer> sorted = lotto.getSortedNumbers();
+
+        // then
+        assertThat(sorted).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
 }
