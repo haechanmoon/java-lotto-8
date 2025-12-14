a8f1ce75d2a0a7d61729463f047d06e0d9cca330
haechanmoon <timmymoon@naver.com>
2025-12-14 20:54:54 +0900

    feat(OutputView) : 로또 입력값 요구 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import lotto.utils.Messages;
+
+public class OutputView {
+
+    public static void printInputMoney() {
+        System.out.println(Messages.ASK_INPUT_MONEY);
+    }
+}
