c5b243814183032efd66240541bba96a6af9812e
haechanmoon <timmymoon@naver.com>
2025-12-18 21:41:57 +0900

    test(ValidatorTest) : 입력값이 숫자가 아닐 때 예외처리 확인

--- src/test/java/lotto/utils/ValidatorTest.java
+++ src/test/java/lotto/utils/ValidatorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
     @Test
@@ -16,5 +18,14 @@ class ValidatorTest {
                 .hasMessageContaining(Messages.ERROR_IS_EMPTY);
     }
 
-    
+    @ParameterizedTest
+    @ValueSource(strings = {"aa", "1b", "2o"})
+    @DisplayName("입력값이 숫자가 아닐 때 예외처리 확인")
+    void 입력값이_숫자가_아닐_때(String input) {
+        assertThatThrownBy(() -> Validator.validateNotDigit(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Messages.ERROR_NOT_DIGIT);
+    }
+
+
 }
